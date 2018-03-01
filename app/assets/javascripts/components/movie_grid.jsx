282ce40b4885ca6d0a324cdc16faee006cc6a51c
zhenjun115 <424087283@qq.com>
2018-03-01 11:22:28 +0800

    布局调整;

--- app/assets/javascripts/components/movie_grid.jsx
+++ app/assets/javascripts/components/movie_grid.jsx
@@ -97,24 +97,6 @@ class MovieGrid extends React.Component {
 							</div>
 						</div>
 					</li>
-					<li>
-						<div>
-							<p>2017</p>
-						</div>
-						<div>
-							<div>
-								<p className="MovieName">logan</p>
-								<p className="MovieTag">
-									<span className="MovieTag">action</span>
-									<span className="MovieTag">adventure</span>
-									<span className="MovieTag">fantasy</span>
-								</p>
-							</div>
-							<div>
-								<p className="MovieScore">4.0</p>
-							</div>
-						</div>
-					</li>
 				</ul>
 			</div>
 		);
